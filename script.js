7eae5d1589a6490f6e645f2e8eb427b6619a4617
yamasaki <yamasaki@example.com>
2026-05-01 12:12:05 +0900

    おみくじの結果を追加

--- script.js
+++ script.js
@@ -2,7 +2,8 @@ const fortunes =[
     "大吉　素敵な出会いがあるかも！",
     "中吉　少し勇気を出すと良いことが起きる",
     "小吉　今日はゆっくり過ごそう",
-    "凶　無理せず慎重に行動しよう"
+    "凶　無理せず慎重に行動しよう",
+    "あったリーーーー！！"
 ];
 
 const button = document.getElementById("omikuji-bt");
