c4a77eec1e38c8da5edbbf1a666fe8c6aa50e255
yamasaki <yamasaki@example.com>
2026-05-01 12:41:28 +0900

    結果で文字色を変更

--- script.js
+++ script.js
@@ -11,6 +11,15 @@ const result = document.getElementById("result");
 
 button.addEventListener("click", function() {
     const randomNumber = Math.floor(Math.random() * fortunes.length);
+    const resultText = fortunes[(randomNumber)];
 
     result.textContent = fortunes[(randomNumber)];
+
+    if (resultText.includes("大吉")) {
+        result.style.color = "red";
+    } else if (resultText.includes("中吉")) {
+        result.style.color = "green";
+    } else {
+        result.style.color = "black";
+    }
 });
